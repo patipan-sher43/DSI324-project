93caf159977483cb6e5624c1e294d2a614421814
patipan-sher43 <103925358+patipan-sher43@users.noreply.github.com>
2022-05-14 15:37:07 +0700

    Update db_custom_init.sh

--- mysql/db_custom_init.sh
+++ mysql/db_custom_init.sh
@@ -1,2 +1,2 @@
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e 'create database curriculum_dataset'
-mysql -u root -p$MYSQL_ROOT_PASSWORD curriculum_dataset < Data.sql
+mysql -u root -p$MYSQL_ROOT_PASSWORD curriculum_dataset < /home/Data.sql
